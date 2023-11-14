f0b3fc6dce87e7df88bfabe0f7fd96e72506d7e3
Manisha061099 <150797683+Manisha061099@users.noreply.github.com>
2023-11-14 17:12:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 df=pd.read_csv("data.csv")
-st.markdown('<span style="color:red">"Manisha Data Analytics"</span>',unsafe_allow_html=True)
+st.markdown('<span style="color:red">Manisha Data Analytics</span>',unsafe_allow_html=True)
 
 if st.sidebar.button("load dataset"):
     st.write(df)
