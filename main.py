027eaa78a381e5c7112a9246682696e877a46366
Manisha061099 <150797683+Manisha061099@users.noreply.github.com>
2023-11-24 10:45:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ if st.sidebar.button("load dataset"):
     st.write(df)
 if st.sidebar.button("show graph"):
     df2=df.head()
-    fig=plt.figure(figsize=(10,8))
+    fig=plt.figure(figsize=(20,8))
     plt.bar(df2['sl'],df2["Trade"])
     st.pyplot(fig)
 if st.sidebar.button("scatter plot"):
     df2=df.head()
-    fig=plt.figure(figsize=(10,8))
+    fig=plt.figure(figsize=(20,8))
     plt.scatter(df2['sl'],df2["Trade"])
     plt.xlabel("sl",fontsize=15)
     plt.ylabel("Trade",fontsize=15)
